22649e53ae89a952ed37ff54b562e750d5dcfc44
Marijn Ophorst <marijn@sensimedia.nl>
2017-06-27 16:59:30 +0200

    docblock/type hints

--- src/Validate/Test.php
+++ src/Validate/Test.php
@@ -2,9 +2,19 @@
 
 namespace Monolyth\Formulaic\Validate;
 
+use Monolyth\Formulaic\Element;
+
 trait Test
 {
-    public function addTest($name, callable $fn)
+    /**
+     * Generic test adder.
+     *
+     * @param string $name Unique name of the test.
+     * @param callable $fn Test function, called with the element's current
+     *  value as argument. Should return true if the test passes, else false.
+     * @return self
+     */
+    public function addTest($name, callable $fn) : Element
     {
         $this->tests[$name] = function ($value) use ($name, $fn) {
             if (is_string($value) && !strlen(trim($value))) {
