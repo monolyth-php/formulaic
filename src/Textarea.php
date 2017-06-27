397d7d89c2427227fef3efad60b9370ae8993f9b
Marijn Ophorst <marijn@sensimedia.nl>
2017-06-27 16:53:07 +0200

    docblock/type hint

--- src/Textarea.php
+++ src/Textarea.php
@@ -4,13 +4,23 @@ namespace Monolyth\Formulaic;
 
 class Textarea extends Element
 {
-    public function __construct($name = null)
+    /**
+     * Constructor.
+     *
+     * @param string $name
+     */
+    public function __construct(string $name)
     {
         parent::__construct($name);
         unset($this->attributes['value']);
     }
 
-    public function __toString()
+    /**
+     * Returns string representation of textarea.
+     *
+     * @return string
+     */
+    public function __toString() : string
     {
         return $this->htmlBefore
             .'<textarea'.$this->attributes().'>'
@@ -25,7 +35,7 @@ class Textarea extends Element
      * @param integer $length Max characters
      * @return self
      */
-    public function maxLength($length)
+    public function maxLength(int $length) : Element
     {
         $this->attributes['maxlength'] = (int)$length;
         return $this->addTest('maxlength', function($value) use ($length) {
