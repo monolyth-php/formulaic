51f3e8635132ffa9500a38906ecdb5787ce13631
Marijn Ophorst <marijn@monomelodies.nl>
2017-06-28 07:39:30 +0200

    toastify

--- tests/elements.php
+++ tests/elements.php
@@ -1,72 +1,53 @@
 <?php
 
-namespace Monolyth\Formulaic\Test;
-
 use Monolyth\Formulaic;
 
 /**
  * Element-specific tests
  */
-class ElementTest
-{
+return function () : Generator {
     /**
-     * Elements without conditions are always valid {?}, whilst required
-     * elements must have a value {?} or validation will fail {?}.
+     * Elements without conditions are always valid, whilst required
+     * elements must have a value or validation will fail
      */
-    public function testDefaultTests()
-    {
+    yield function () {
         $input = new Formulaic\Text('test');
-        yield assert($input->valid());
+        assert($input->valid());
 
         // Required:
         $input->isRequired();
         $input->setValue('foo');
-        yield assert($input->valid());
+        assert($input->valid());
         $input->setValue(null);
-        yield assert($input->valid() != true);
-    }
+        assert($input->valid() != true);
+    };
 
-    /**
-     * Generic buttons.
-     */
-    public function testButton()
-    {
+    /** Generic buttons */
+    yield function () {
         $button = new Formulaic\Button('B');
-        yield assert("$button" == '<button type="button">B</button>');
-    }
+        assert("$button" == '<button type="button">B</button>');
+    };
 
-    /**
-     * Reset buttons.
-     */
-    public function testButtonReset()
-    {
+    /** Reset buttons */
+    yield function () {
         $button = new Formulaic\Button\Reset('B');
-        yield assert("$button" == '<button type="reset">B</button>');
-    }
+        assert("$button" == '<button type="reset">B</button>');
+    };
 
-    /**
-     * Submit buttons.
-     */
-    public function testButtonSubmit()
-    {
+    /** Submit buttons */
+    yield function () {
         $button = new Formulaic\Button\Submit('B');
-        yield assert("$button" == '<button type="submit">B</button>');
-    }
+        assert("$button" == '<button type="submit">B</button>');
+    };
 
-    /**
-     * Checkboxes.
-     */
-    public function testCheckbox()
-    {
+    /** Checkboxes */
+    yield function () {
         $input = new Formulaic\Checkbox('test');
-        yield assert("$input" == '<input id="test-1" name="test" type="checkbox" value="1">');
-    }
+        assert("$input" == '<input id="test-1" name="test" type="checkbox" value="1">');
+    };
 
-    /**
-     * Checkbox groups.
-     */
-    public function testCheckboxGroup()
-    {
+    /** Checkbox groups */
+    yield function () {
         $out = <<<EOT
 <div>
 <label for="test-1"><input id="test-1" name="test[]" type="checkbox" value="1"> Option 1</label>
@@ -80,244 +61,196 @@ EOT;
                 2 => 'Option 2',
             ]
         );
-        yield assert("$group" == $out);
-    }
+        assert("$group" == $out);
+    };
 
-    /**
-     * Dates.
-     */
-    public function testDate()
-    {
+    /** Dates */
+    yield function () {
         $input = new Formulaic\Date('test');
-        yield assert("$input" == '<input id="test" name="test" type="date">');
+        assert("$input" == '<input id="test" name="test" type="date">');
         $input->setMin('2010-01-01')->setMax('2012-01-01');
         $input->setValue('2009-01-01');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue('2013-01-01');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue('2011-01-01');
-        yield assert($input->valid());
+        assert($input->valid());
         $input->setValue('something illegal');
-        yield assert($input->valid() != true);
-    }
+        assert($input->valid() != true);
+    };
 
-    /**
-     * Datetime.
-     */
-    public function testDatetime()
-    {
+    /** Datetime */
+    yield function () {
         $input = new Formulaic\Datetime('test');
-        yield assert("$input" == '<input id="test" name="test" type="datetime">');
+        assert("$input" == '<input id="test" name="test" type="datetime">');
         $input->setMin('2009-01-01 12:00:00')->setMax('2009-01-02 12:00:00');
         $input->setValue('2009-01-01');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue('2009-01-02 13:00:00');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue('2009-01-01 13:59:00');
-        yield assert($input->valid());
+        assert($input->valid());
         $input->setValue('something illegal');
-        yield assert($input->valid() != true);
-    }
+        assert($input->valid() != true);
+    };
 
-    /**
-     * Email.
-     */
-    public function testEmail()
-    {
+    /** Email */
+    yield function () {
         $input = new Formulaic\Email('test');
-        yield assert("$input" == '<input id="test" name="test" type="email">');
+        assert("$input" == '<input id="test" name="test" type="email">');
         $input->setValue('not an email');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue('foo@bar.com');
-        yield assert($input->valid());
-    }
+        assert($input->valid());
+    };
 
-    /**
-     * Files.
-     */
-    public function testFile()
-    {
+    /** Files */
+    yield function () {
         $input = new Formulaic\File('test');
-        yield assert("$input" == '<input id="test" name="test" type="file">');
-    }
+        assert("$input" == '<input id="test" name="test" type="file">');
+    };
 
-    /**
-     * Hidden inputs.
-     */
-    public function testHidden()
-    {
+    /** Hidden inputs */
+    yield function () {
         $input = new Formulaic\Hidden('test');
-        yield assert("$input" == '<input id="test" name="test" type="hidden">');
-    }
+        assert("$input" == '<input id="test" name="test" type="hidden">');
+    };
 
-    /**
-     * Numbers.
-     */
-    public function testNumber()
-    {
+    /** Numbers */
+    yield function () {
         $input = new Formulaic\Number('test');
         $input->setValue('42');
-        yield assert("$input" == '<input id="test" name="test" step="1" type="number" value="42">');
+        assert("$input" == '<input id="test" name="test" step="1" type="number" value="42">');
         $input->setValue('foo');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setMin(9);
         $input->setValue(8);
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue(15);
-        yield assert($input->valid());
+        assert($input->valid());
         $input->setMax(14);
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue(13);
-        yield assert($input->valid());
+        assert($input->valid());
         $input->setStep(2);
-        yield assert($input->valid());
+        assert($input->valid());
         $input->setValue(12);
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setStep(.5);
         $input->setValue(12.5);
-        yield assert($input->valid());
+        assert($input->valid());
         $input->setValue(12.4);
-        yield assert($input->valid() != true);
-    }
+        assert($input->valid() != true);
+    };
 
-    /**
-     * Passwords.
-     */
-    public function testPassword()
-    {
+    /** Passwords */
+    yield function () {
         $input = new Formulaic\Password('test');
         $input->setValue('secret');
-        yield assert("$input" == '<input id="test" name="test" type="password">');
-    }
+        assert("$input" == '<input id="test" name="test" type="password">');
+    };
 
-    /**
-     * Radio buttons.
-     */
-    public function testRadio()
-    {
+    /** Radio buttons */
+    yield function () {
         $input = new Formulaic\Radio('test');
-        yield assert("$input" == '<input id="test" name="test" type="radio" value="1">');
-    }
+        assert("$input" == '<input id="test" name="test" type="radio" value="1">');
+    };
 
-    /**
-     * Search boxes.
-     */
-    public function testSearch()
-    {
+    /** Search boxes */
+    yield function () {
         $input = new Formulaic\Search('test');
-        yield assert("$input" == '<input id="test" name="test" type="search">');
-    }
+        assert("$input" == '<input id="test" name="test" type="search">');
+    };
 
-    /**
-     * Simple select boxes.
-     */
-    public function testSelectSimple()
-    {
+    /** Simple select boxes */
+    yield function () {
         $input = new Formulaic\Select('test', [1 => 'foo', 2 => 'bar']);
-        yield assert("$input" == <<<EOT
+        assert("$input" == <<<EOT
 <select id="test" name="test">
 <option value="1">foo</option>
 <option value="2">bar</option>
 </select>
 EOT
         );
-    }
+    };
 
-    /**
-     * Manually built select boxes.
-     */
-    public function testSelectManual()
-    {
+    /** Manually built select boxes */
+    yield function () {
         $input = new Formulaic\Select('test', function ($select) {
             $select[] = new Formulaic\Select\Option(1, 'foo');
             $select[] = new Formulaic\Select\Option(2, 'bar');
         });
-        yield assert("$input" == <<<EOT
+        assert("$input" == <<<EOT
 <select id="test" name="test">
 <option value="1">foo</option>
 <option value="2">bar</option>
 </select>
 EOT
         );
-    }
+    };
     
-    /**
-     * Telephone numbers.
-     */
-    public function testTel()
-    {
+    /** Telephone numbers */
+    yield function () {
         $input = new Formulaic\Tel('test');
         $input->setValue('612345678');
-        yield assert("$input" == '<input id="test" name="test" type="tel" value="0612345678">');
+        assert("$input" == '<input id="test" name="test" type="tel" value="0612345678">');
         $input->setValue('foo');
-        yield assert($input->valid() != true);
-    }
+        assert($input->valid() != true);
+    };
 
-    /**
-     * Text elements with HTML encoded values.
-     */
-    public function testText()
-    {
+    /** Text elements with HTML encoded values */
+    yield function () {
         $input = new Formulaic\Text('test');
         $input->setValue('"');
-        yield assert("$input" == '<input id="test" name="test" type="text" value="&quot;">');
-    }
+        assert("$input" == '<input id="test" name="test" type="text" value="&quot;">');
+    };
 
-    /**
-     * Textareas with HTML encoded values.
-     */
-    public function testTextarea()
-    {
+    /** Textareas with HTML encoded values */
+    yield function () {
         $input = new Formulaic\Textarea('test');
         $input->setValue('"');
-        yield assert("$input" == '<textarea id="test" name="test">&quot;</textarea>');
-    }
+        assert("$input" == '<textarea id="test" name="test">&quot;</textarea>');
+    };
 
-    /**
-     * Time elements.
-     */
-    public function testTime()
-    {
+    /** Time elements */
+    yield function () {
         $input = new Formulaic\Time('test');
         $input->setValue('bla');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue('12:00:00');
-        yield assert($input->valid());
-        yield assert("$input" == '<input id="test" name="test" type="time" value="12:00:00">');
+        assert($input->valid());
+        assert("$input" == '<input id="test" name="test" type="time" value="12:00:00">');
 
         // Test require past time
         $input = new Formulaic\Time('test');
         $input->isInPast();
         $input->setValue(time() + 100);
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue(time() - 100);
-        yield assert($input->valid());
+        assert($input->valid());
 
         // Test require future time
         $input = new Formulaic\Time('test');
         $input->isInFuture();
         $input->setValue(time() - 100);
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input->setValue(time() + 100);
-        yield assert($input->valid());
-    }
+        assert($input->valid());
+    };
 
-    /**
-     * URLs.
-     */
-    public function testUrl()
-    {
+    /** URLs */
+    yield function () {
         $input = new Formulaic\Url('test');
         $input->setValue('not an url');
-        yield assert($input->valid() != true);
+        assert($input->valid() != true);
         $input2 = new Formulaic\Url('test');
         $input2->setValue('http://google.com');
-        yield assert($input2->valid());
-        yield assert("$input\n$input2" == <<<EOT
+        assert($input2->valid());
+        assert("$input\n$input2" == <<<EOT
 <input id="test" name="test" placeholder="http://" type="url" value="http://not an url">
 <input id="test" name="test" placeholder="http://" type="url" value="http://google.com">
 EOT
         );
-    }
-}
+    };
+};
 
