af8201b1e412f10bd70e965c09abf9ebc1136723
Marijn Ophorst <marijn@monomelodies.nl>
2018-01-27 19:09:34 +0100

    no reference

--- src/Element/Group.php
+++ src/Element/Group.php
@@ -127,7 +127,7 @@ class Group extends ArrayObject implements JsonSerializable
      *
      * @return array
      */
-    public function & getValue() : array
+    public function getValue() : array
     {
         $this->value = [];
         foreach ((array)$this as $field) {
