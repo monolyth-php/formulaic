3147b80565d87af3460d49319d18df0e322c2300
Marijn Ophorst <marijn@monomelodies.nl>
2018-10-19 17:16:04 +0200

    doccomment

--- src/Checkbox.php
+++ src/Checkbox.php
@@ -4,6 +4,11 @@ namespace Monolyth\Formulaic;
 
 class Checkbox extends Radio
 {
+    /**
+     * @var array
+     *
+     * Hash of attributes.
+     */
     protected $attributes = ['type' => 'checkbox'];
 }
 
