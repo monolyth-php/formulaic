15c93d7623b4c3768aaf21c714d9cc58185a3599
Marijn Ophorst <marijn@monomelodies.nl>
2018-10-19 17:16:41 +0200

    doccomment

--- src/Date.php
+++ src/Date.php
@@ -4,7 +4,18 @@ namespace Monolyth\Formulaic;
 
 class Date extends Datetime
 {
+    /**
+     * @var array
+     *
+     * Hash of attributes.
+     */
     protected $attributes = ['type' => 'date'];
+
+    /**
+     * @var string
+     *
+     * The default format.
+     */
     protected $format = 'Y-m-d';
 }
 
