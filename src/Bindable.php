78e76d6634d59b7a5f604b4f0c45fae8d61d0c2f
Marijn Ophorst <marijn@monomelodies.nl>
2022-09-11 13:23:19 +0200

    hm, that kinda broke stuff

--- src/Bindable.php
+++ src/Bindable.php
@@ -108,7 +108,6 @@ EOT
                         $model->$name = $element->checked();
                     } else {
                         $element->setValue($curr);
-                        $model->$name = $curr;
                     }
                 }
                 $element->bind($model);
