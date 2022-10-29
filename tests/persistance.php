790dfa8fe76f57664172f6d86bec231aa9cd7fec
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-29 16:15:41 +0200

    wrap correctly

--- tests/persistance.php
+++ tests/persistance.php
@@ -5,21 +5,18 @@ use Gentry\Gentry\Wrapper;
 
 /** Data persistance */
 return function () : Generator {
-    /**
-     * After setting a value and binding a model the model gets updated
-     * if the element changes
-     */
+    /** After setting a value and binding a model the model gets updated if the element was posted */
     yield function () {
         $_POST['name'] = 'Linus';
         $user = new class {
             public $name = 'Marijn';
         };
         assert('Marijn' == $user->name);
-        $form = new class extends Formulaic\Post {
+        $form = new Wrapper(new class extends Formulaic\Post {
             public function __construct() {
-                $this[] = new Wrapper(new Formulaic\Text('name'));
+                $this[] = new Formulaic\Text('name');
             }
-        };
+        });
         $form->bind($user);
         assert('Linus' == $user->name);
         $form['name']->getElement()->setValue('Chuck Norris');
