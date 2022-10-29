a0fdf236ddba86ccc9b17925f690b3f9659c3001
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-29 15:32:17 +0200

    add wrapping

--- tests/labels.php
+++ tests/labels.php
@@ -1,6 +1,7 @@
 <?php
 
 use Monolyth\Formulaic;
+use Gentry\Gentry\Wrapper;
 
 /**
  * Label tests
@@ -11,7 +12,7 @@ return function () : Generator {
      */
     yield function () {
         $input = new Formulaic\Text('test');
-        $label = new Formulaic\Label('Label', $input);
+        $label = new Wrapper(new Formulaic\Label('Label', $input));
         yield assert("$label" == <<<EOT
 <label for="test">Label</label>
 <input id="test" name="test" type="text">
