0f54ea4768d552ee5ec37d76b502a8221c59d62e
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-30 21:25:31 +0100

    move tostring here

--- src/Select.php
+++ src/Select.php
@@ -142,5 +142,49 @@ class Select extends ArrayObject implements Labelable, Testable, Bindable
         }
         return $this;
     }
+
+    public function __toString()
+    {
+        if (isset($this->attributes['name'])
+            && !is_bool($this->attributes['name'])
+        ) {
+            $old = $this->attributes['name'];
+            $parts = $this->prefix;
+            $parts[] = $old;
+            $start = array_shift($parts);
+            $this->attributes['name'] = $start;
+            foreach ($parts as $part) {
+                if (!is_bool($part)) {
+                    $this->attributes['name'] .= "[$part]";
+                }
+            }
+        }
+        if (isset($this->attributes['name'])
+            && is_bool($this->attributes['name'])
+        ) {
+            $old = $this->attributes['name'];
+            unset($this->attributes['name']);
+        }
+        if ($id = $this->id()) {
+            if (isset($this->idPrefix)) {
+                $id = "{$this->idPrefix}-$id";
+            }
+            $this->attributes['id'] = $id;
+        }
+        $out = $this->htmlBefore ?? '';
+        $out .= '<select'.$this->attributes().'>';
+        if (count((array)$this)) {
+            $out .= "\n";
+            foreach ((array)$this as $option) {
+                $out .= "$option";
+            }
+        }
+        $out .= "</select>\n";
+        $out .= $this->htmlAfter ?? '';
+        if (isset($old)) {
+            $this->attributes['name'] = $old;
+        }
+        return $out;
+    }
 }
 
