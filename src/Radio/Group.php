c24221b8bfc0ac21bf33bead8e5ff287f1197f89
Marijn Ophorst <marijn@monomelodies.nl>
2022-10-31 10:26:32 +0100

    this is just the element group toString, essentially

--- src/Radio/Group.php
+++ src/Radio/Group.php
@@ -156,35 +156,5 @@ class Group extends Element\Group implements Labelable, Testable
         }
         return $this;
     }
-
-    /**
-     * Returns a rendered version of this radio group.
-     *
-     * @return string
-     */
-    public function __toString() : string
-    {
-        $out = '';
-        if ($this->htmlGroup & self::WRAP_GROUP) {
-            $out .= $this->htmlBefore;
-        }
-        foreach ((array)$this as $field) {
-            if ($this->htmlGroup & self::WRAP_LABEL) {
-                $field->wrap($this->htmlBefore, $this->htmlAfter);
-            }
-            if ($this->htmlGroup & self::WRAP_ELEMENT) {
-                $field->getElement()->wrap($this->htmlBefore, $this->htmlAfter);
-            }
-        }
-        if (count((array)$this)) {
-            foreach ((array)$this as $element) {
-                $out .= "$element\n";
-            }
-        }
-        if ($this->htmlGroup & self::WRAP_GROUP) {
-            $out .= $this->htmlAfter;
-        }
-        return $out;
-    }
 }
 
