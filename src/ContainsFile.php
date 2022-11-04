03a8918be18873dc21416e9410461da9af6a1bcd
Marijn Ophorst <marijn@monomelodies.nl>
2022-11-04 17:25:15 +0100

    this must of course be public

--- src/ContainsFile.php
+++ src/ContainsFile.php
@@ -4,7 +4,7 @@ namespace Monolyth\Formulaic;
 
 trait ContainsFile
 {
-    private function containsFile() : bool
+    public function containsFile() : bool
     {
         foreach ($this as $element) {
             if ($element->getElement() instanceof File || ($element->getElement() instanceof Element\Group && $element->containsFile())) {
