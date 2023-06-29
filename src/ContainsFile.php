de9aa194accc8e61613f203941ef4d041978b08a
Marijn Ophorst <marijn@sensimedia.nl>
2023-06-29 16:29:27 +0200

    these might have strings

--- src/ContainsFile.php
+++ src/ContainsFile.php
@@ -7,7 +7,9 @@ trait ContainsFile
     public function containsFile() : bool
     {
         foreach ($this as $element) {
-            if ($element->getElement() instanceof File || ($element->getElement() instanceof Element\Group && $element->containsFile())) {
+            if (is_object($element)
+                && ($element->getElement() instanceof File || ($element->getElement() instanceof Element\Group && $element->containsFile()))
+            ) {
                 return true;
             }
         }
